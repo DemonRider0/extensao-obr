b684bc715911f3a9217c0280210f9b81ea92a073
DemonRider0 <michaeldouglasa65@gmail.com>
2026-04-23 14:12:13 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-import OBR from "https://unpkg.com/@owlbear-rodeo/sdk?module";
+import OBR from "https://cdn.jsdelivr.net/npm/@owlbear-rodeo/sdk/+esm";
 
 OBR.onReady(() => {
   console.log("Extensão carregada!");
 
-  document.getElementById("btn").addEventListener("click", () => {
-    OBR.notification.show("Funcionou!");
+  document.getElementById("btn").addEventListener("click", async () => {
+    await OBR.notification.show("Funcionou!");
   });
 });
